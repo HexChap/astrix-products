a7b04c4a822a2aee77223289fdc175a1f659ae2f
HexChap <hexchap@tutanota.com>
2021-12-21 15:56:51 +0200

    Небольшие изменения в тексте

--- app/core/strings/technical_info.py
+++ app/core/strings/technical_info.py
@@ -6,8 +6,8 @@ from app.core.config import config
 class TecnicalInfo:
     welcome_message = (
         "Бот онлайн!\n" 
-        f"Узернейм бота: @{config.bot.username}\n"
+        f"Никнейм бота: @{config.bot.username}\n"
         f"ID бота: {config.bot.id}\n" 
-        f"Развернут на фрейворке: {telegram.__name__} " 
+        f"Развернут на фреймворке: {telegram.__name__} " 
         f"v{telegram.__version__}\n"
     )
